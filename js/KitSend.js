b61a6ceea94beba1c7b56f3f63391b9acce7da62
Роман <>
2018-09-12 18:43:18 +0700

    mac

--- js/KitSend.js
+++ js/KitSend.js
@@ -82,6 +82,9 @@ $(document).ready(function(){
 	            	locked: true 
 	         	}
 	      	},
+	      	btnTpl : {
+		    	smallBtn   : '<button data-fancybox-close class="fancybox-close-small icon-close" title="Закрыть"></button>'
+		    },
 			beforeShow: function(){
 				$(".fancybox-wrap").addClass("beforeShow");
 				$popup.find(".custom-field").remove();
